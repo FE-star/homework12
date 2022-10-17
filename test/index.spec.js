8edc95027ee15eb46c576c443784f15941552d05
sj-henryxue <shangjian@henrydeMacBook-Pro.local>
2022-10-17 18:49:30 +0800

    feat:提交作业

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,84 +1,151 @@
-describe('初始化路由', () => {
-  it('default route', () => {
+const Router = require("../router");
+
+describe("初始化路由", () => {
+  it("default route", () => {
     //路由表
-    var routes = [{
-      id: ''
-    }, {
-      id: 'index',
-      title: '首页'
-    }, {
-      id: 'next',
-      title: '下一页'
-    }]
+    var routes = [
+      {
+        id: "",
+      },
+      {
+        id: "index",
+        title: "首页",
+      },
+      {
+        id: "next",
+        title: "下一页",
+      },
+    ];
 
     //路由器
-    var router = new Router(routes, 'next');
-    expect(router.currentRoute).toMatchObject({ id: 'next', title: '下一页' })
+    var router = new Router(routes, "next");
+    expect(router.currentRoute).toMatchObject({ id: "next", title: "下一页" });
   });
 
-  it('route push', () => {
+  it("route push", () => {
     //路由表
-    var routes = [{
-      id: ''
-    }, {
-      id: 'index',
-      title: '首页'
-    }, {
-      id: 'next',
-      title: '下一页'
-    }]
+    var routes = [
+      {
+        id: "",
+      },
+      {
+        id: "index",
+        title: "首页",
+      },
+      {
+        id: "next",
+        title: "下一页",
+      },
+    ];
 
     //路由器
-    var router = new Router(routes, 'index');
-    const res = router.push('next', () => { return 'push next'; });
-    expect(res).toBe('push next');
+    var router = new Router(routes, "index");
+    const res = router.push("next", () => {
+      return "push next";
+    });
+    expect(res).toBe("push next");
     expect(router.currentRoute).toMatchObject({
-      id: 'next',
-      title: '下一页'
+      id: "next",
+      title: "下一页",
     });
   });
 
-  it('route back', () => {
+  it("route back", async () => {
     //路由表
-    var routes = [{
-      id: ''
-    }, {
-      id: 'index',
-      title: '首页'
-    }, {
-      id: 'next',
-      title: '下一页'
-    }]
+    var routes = [
+      {
+        id: "",
+      },
+      {
+        id: "index",
+        title: "首页",
+      },
+      {
+        id: "next",
+        title: "下一页",
+      },
+      {
+        id: "about",
+        title: "关于",
+      },
+    ];
 
     //路由器
-    var router = new Router(routes, 'index');
-    router.push('next');
+    var router = new Router(routes, "index");
+    router.push("next");
+
     router.back();
     expect(router.currentRoute).toMatchObject({
-      id: 'index',
-      title: '首页'
+      id: "index",
+      title: "首页",
+    });
+
+    router.forward();
+    expect(router.currentRoute).toMatchObject({
+      id: "next",
+      title: "下一页",
+    });
+
+    router.back();
+    expect(router.currentRoute).toMatchObject({
+      id: "index",
+      title: "首页",
+    });
+
+    router.back();
+    expect(router.currentRoute).toMatchObject({
+      id: "",
+    });
+
+    router.forward();
+    expect(router.currentRoute).toMatchObject({
+      id: "index",
+      title: "首页",
+    });
+    router.forward();
+    expect(router.currentRoute).toMatchObject({
+      id: "next",
+      title: "下一页",
+    });
+
+    router.back();
+    router.push("about");
+    router.back();
+    router.forward();
+    expect(router.currentRoute).toMatchObject({
+      id: "about",
+      title: "关于",
+    });
+    router.forward();
+    expect(router.currentRoute).toMatchObject({
+      id: "about",
+      title: "关于",
     });
   });
 
-  it('route replace', () => {
+  it("route replace", () => {
     //路由表
-    var routes = [{
-      id: ''
-    }, {
-      id: 'index',
-      title: '首页'
-    }, {
-      id: 'next',
-      title: '下一页'
-    }]
+    var routes = [
+      {
+        id: "",
+      },
+      {
+        id: "index",
+        title: "首页",
+      },
+      {
+        id: "next",
+        title: "下一页",
+      },
+    ];
 
     //路由器
-    var router = new Router(routes, 'index');
-    router.push('next');
-    router.replace('index');
+    var router = new Router(routes, "index");
+    router.push("next");
+    router.replace("index");
     expect(router.currentRoute).toMatchObject({
-      id: 'index',
-      title: '首页'
+      id: "index",
+      title: "首页",
     });
   });
 });
