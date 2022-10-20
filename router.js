8b40bdebf2b5d15270f073f61e3ba971167b034a
chenli <chenl@homeking365.com>
2022-10-20 13:22:09 +0800

    feat: 提交作业

--- router.js
+++ router.js
@@ -1,17 +1,30 @@
 function Router(routes, defaultRoute) {
   //TODO
+  this.init(routes, defaultRoute)
 }
 
 //初始化，可多次初始化
 Router.prototype.init = function(routes, defaultRoute) {
   //TODO
-  this.currentRoute
-  this.oldRoute
+  this.routes = routes
+  this.routeMap = new Map(routes.map((route) => (
+    [route.id, route]
+  )))
+  this.stack = []
+  this.push(defaultRoute)
+  this.oldRoute = null
 }
 
 //切换路由
 Router.prototype.push = function(route, callback) {
   //TODO
+  this.currentRoute = this.routeMap.get(route)
+
+  if (this.currentRoute) {
+    this.stack.push(this.currentRoute)
+  }
+
+  return callback && callback()
 }
 
 //前进
@@ -21,6 +34,15 @@ Router.prototype.forward = function () {
 
 //后退
 Router.prototype.back = function () {
+  this.stack.pop()
+  this.currentRoute = this.stack[this.stack.length - 1]
+  history.back();
+}
+
+//替换
+Router.prototype.replace = function (route) {
+  this.stack.pop()
+  this.push(route)
   history.back();
 }
 
